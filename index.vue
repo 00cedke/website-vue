ce3af06c57e2fa91d241f6e838599fe058acda69
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-07-04 23:14:25 +0200

    Update index.vue

--- index.vue
+++ index.vue
@@ -1,1 +1,22 @@
+<template>
+  <div class="msg">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Welcome to my website made in vue !!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.msg {
+  color: aquamarine;
+}
+</style>
 
