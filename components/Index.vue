6815fdfd0cf1d5f92b77fd98c8ec88abcc63ba9b
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-07-04 23:16:20 +0200

    Create Index.vue

---
+++ components/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="msg">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Welcome to my website made in vue !!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.msg {
+  color: aquamarine;
+}
+</style>
